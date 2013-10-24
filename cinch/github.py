12ea857e0a4be73017b5e5b43d1e4a0eca6e8844
junkafarian <junkafarian@gmail.com>
2013-10-24 18:57:57 +0100

    fleshing out class structure

--- cinch/github.py
+++ cinch/github.py
@@ -3,6 +3,7 @@ from flask import request
 from github import Github, UnknownObjectException
 
 from cinch import app
+from cinch.check import check, get_checks
 
 logger = logging.getLogger(__name__)
 
@@ -24,29 +25,70 @@ class GithubAdapter(object):
     def __init__(self, gh):
         self.gh = gh
 
-    def get_repo(self, project):
-        repo_name = PROJECT_CONFIG.get(project)
+    _repo = None
 
-        try:
-            repo = self.gh.get_repo(repo_name)
-        except UnknownObjectException:
-            repo = None
+    @property
+    def repo(self):
+        if self._repo is None:
+            try:
+                owner_name = self.data['owner']['name']
+                repo_name = self.data['repository']['name']
+            except KeyError:
+                logger.error(
+                    'Unable to parse data. Malformed request:\n'
+                    '{}'.format(self.data))
+                return
 
-        return repo
+            self.repo_name = '{}/{}'.format(owner_name, repo_name)
+
+            try:
+                self._repo = self.gh.get_repo(repo_name)
+            except UnknownObjectException:
+                self._repo = None
+
+        return self._repo
 
     def handle_update(self, data):
-        try:
-            owner_name = data['owner']['name']
-            repo_name = data['repository']['name']
-        except KeyError:
-            logger.error('Unable to parse data. Malformed request: {}'.format(data))
-        repo = self.get_repo('{}/{}'.format(owner_name, repo_name))
+        self.data = data
 
-        if repo is None:
-            logger.warning('received webhook for unconfigured project: {}'.format(repo_name))
+        if self.repo is None:
+            logger.warning(
+                'received webhook for unconfigured project:\n'
+                '{}'.format(data))
+
+        # TODO: determine whether this update was triggered by a pull
+        #       request or master (aka. if we're interested)
+
+        # Perform checks
+        for check_method in get_checks(self):
+            check_method()
+
+    @check
+    def up_to_date(self):
+        # Find out the current master SHA. Consider using local git to find
+        # this to avoid the api call
+        master_sha = self.repo.get_branch('master').commit.sha
+
+        head_sha = self.data.get('after')
+
+        try:
+            status = self.repo.compare(master_sha, head_sha)
+        except (UnknownObjectException, AssertionError):
+            logger.warning(
+                'unable to perform up_to_date check between commits: '
+                '{}, {}'.format(master_sha, head_sha))
             return
 
-        # Do some processing
+        if status.behind_by > 0:
+            # set flag to false
+            pass
+        elif status.ahead_by <= 0:
+            # pull request has been merged
+            pass
+        else:
+            # Pull Request is ahead of master and up to date.
+            # It's still active
+            pass
 
 
 @app.route('/api/github/pull', methods=['POST'])

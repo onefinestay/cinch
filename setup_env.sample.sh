f2beff34be8714c4536e0e81754c0a03f86fc092
junkafarian <junkafarian@gmail.com>
2013-12-02 13:48:29 +0000

    added setup_env template

---
+++ setup_env.sample.sh
@@ -0,0 +1,13 @@
+# Required for authenticating users with github.
+# see https://github-flask.readthedocs.org/en/latest/
+export GITHUB_CLIENT_ID=
+export GITHUB_CLIENT_SECRET=
+export GITHUB_CALLBACK_URL=
+
+# The token for the application to authenticate with when making
+# requests to the github api
+export GITHUB_TOKEN=
+
+# The secret key required for flask session management.
+# see http://flask.pocoo.org/docs/quickstart/#sessions
+export SECRET_KEY=

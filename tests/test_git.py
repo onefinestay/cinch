d53571c9a747e0246e572fdd43e641469b726598
David Szotten <davidszotten@gmail.com>
2014-05-19 17:56:24 +0100

    basic git tests

---
+++ tests/test_git.py
@@ -0,0 +1,35 @@
+from mock import patch
+import pytest
+
+from cinch.git import Repo
+
+
+@pytest.yield_fixture(scope='module')
+def tmp_base_dir(request):
+    # builtin tmpdir fixture is function scoped :(
+    tmpdir = request.config._tmpdirhandler.mktemp('cinch_repos')
+    with patch('cinch.git.app') as app:
+        app.config = {'REPO_BASE_DIR': tmpdir.strpath}
+        yield
+    tmpdir.remove()
+
+
+@pytest.fixture(scope='module')
+def repo(tmp_base_dir):
+    repo = Repo.setup_repo('onefinestay', 'cinch')
+    return repo
+
+def test_compare(repo):
+    _, ahead = repo.compare_pr(1)
+    assert ahead == 0
+
+def test_compare_unknown(repo):
+    behind, ahead = repo.compare_pr(-1)
+    assert (behind, ahead) == (None, None)
+
+def test_fetch(repo):
+    # nothing to return. should just not brak
+    assert repo.fetch() is None
+
+def test_is_mergeable(repo):
+    assert repo.is_mergeable(1)

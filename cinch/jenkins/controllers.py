2c124e2ca4f2eb0675776fd6d8333c1959dadb7e
David Szotten <davidszotten@gmail.com>
2014-05-21 22:21:34 +0100

    tidy

--- cinch/jenkins/controllers.py
+++ cinch/jenkins/controllers.py
@@ -4,11 +4,10 @@ from collections import OrderedDict
 
 from flask import url_for, g
 from sqlalchemy.orm import joinedload
-# from sqlalchemy.sql.expression import and_
 
 from cinch.check import check, CheckStatus
-from cinch.models import db, Project, Commit
-from .models import Job, Build
+from cinch.models import db, Project, Commit, PullRequest
+from .models import Job, Build, build_commits
 
 
 def g_cache(func):
@@ -88,8 +87,15 @@ def get_jobs(project_name):
         )
 
 
-def has_successful_builds(job, branch_shas):
+@g_cache
+def all_open_prs():
+    return _all_open_prs()
+
+# so we can test without caching
+def _all_open_prs():
     """
+        TODO: update
+
         For a given job and main project, look for builds that are
             1. successful
             2. match the head shas for the projects in that build (unless
@@ -102,156 +108,79 @@ def has_successful_builds(job, branch_shas):
         Return:
             A list of names of successful jobs
     """
-    # it should be possible to do this more efficiently with some
-    # well written sql
 
-    # SHAs to match starts as the master_sha of the relevant projects
-    job_shas = {
-        project.name: project.master_sha
-        for project in job.projects
-    }
-
-    shas = job_shas.copy()
-    # but specific SHAs can be provided to test against
-    shas.update(branch_shas)
-
-    # in case shas from unknown projects were provided, skip them
-    shas = {
-        key: value
-        for key, value in shas.items()
-        if key in job_shas
-    }
-
-    # iterate over all builds of this job. if one matches the exact set
-    # of SHAs we're matching for, consider it a success
-    for build in job.builds:
-        if not build.success:
-            continue
-
-        commits = {
-            commit.project.name: commit.sha
-            for commit in build.commits
-        }
-        if commits == shas:
-            return True
-
-    return False
-
-
-@g_cache
-def all_open_prs():
-    return _all_open_prs()
-
-# so we can test without caching
-def _all_open_prs():
-    import time
-    start = time.time()
-    count = 0
-
-    from cinch.models import PullRequest
-    open_prs = db.session.query(PullRequest
+    open_pull_requests = db.session.query(PullRequest
         ).filter_by(is_open=True
         ).options(
             joinedload('project')
     ).all()
 
     db.session.query(Project).options(joinedload('jobs')).all()
-    # projects = db.session.query(Project).options(joinedload('jobs')).all()
-
-    # projects = db.session.query(Project).all()
-    # jobs = db.session.query(Job).options(joinedload('projects')).all()
-    # project_map = {project.name: project.id for project in projects}
     jobs = db.session.query(Job).options(joinedload('projects')).all()
 
+    # For each job, we construct a query that finds tuples of shas
+    # for all successful builds. We can then match those against the required
+    # shas for any given pull_request (the head for the project of the pr,
+    # and master for any related projects). It may be possible to do this in
+    # a single query, for for my sanity we're doing n queries for now. Note
+    # that n depends on the number of _jobs_, not the number of open
+    # pull requests.
     job_sha_map = {}
+    successful_job_shas = {}
     for job in jobs:
-        job_shas = OrderedDict({
-            project.name: project.master_sha
-            for project in job.projects
-        })
-        job_sha_map[job] = job_shas
+        # The subqueries for the related projects are ordered in
+        # `successful_job_shas` below. This order needs to match
+        # the order use to look them up later for each pull_request.
+        job_sha_map[job] = OrderedDict()
 
-    # interesting_shas = []
-
-
-    # from .models import build_commits
-    from .models import BuildCommits
-
-    # successful_job_shas = {}
-    successful_job_shas2 = {}
-    for job, shas in job_sha_map.items():
-        base_query = db.session.query(Build).filter_by(success=True)
-        subquery = base_query.subquery(name='basequery')
-        query = db.session.query(subquery)
+        base_query = db.session.query(Build).filter_by(
+            success=True).subquery(name='basequery')
+        query = db.session.query(base_query)
         aliases = []
-        # columns2 = [subquery.c.build_number]
 
-        # build_query = db.session.query(Job).filter_by(id=job.id).join(Build).join(build_commits)
+        for project in job.projects:
+            job_sha_map[job][project.name] = project.master_sha
 
-        for project_name, sha in shas.items():
-            # alias = aliased(BuildCommits, name=project_name)
-            build_query = db.session.query(BuildCommits).join(Build).join(Job).filter_by(id=job.id).subquery(name=project_name)
-            # build_query = db.session.query(alias).join(Build).join(Job).filter_by(id=job.id).subquery(name=project_name)
-            # columns2.append(build_query)
-            aliases.append(build_query)
-            # columns2.append(alias.commit_sha)
+            subquery_alias = db.session.query(
+                build_commits
+                ).join(Build).join(Job).filter_by(
+                id=job.id
+                ).subquery(name=project.name)
+            aliases.append(subquery_alias)
 
-
-            # project_id = project_map[project_name]
-            # alias = aliased(build_commits, name=project_name)
-            # alias = alias(build_query)
-            # aliases.append(alias)
             query = query.outerjoin(
-                # alias,
-                build_query,
+                subquery_alias,
             )
 
-        # columns = [alias.c.commit_sha for alias in aliases]
-        # successful_job_shas[job] = set(query.values(*columns))
+        columns = [base_query.c.build_number] + [
+            alias.c.commit_sha for alias in aliases]
 
-        columns2 = [subquery.c.build_number] + [alias.c.commit_sha for alias in aliases]
-        successful_job_shas2[job] = {result[1:]: result[0] for result in query.values(*columns2)}
-
-        # if job.name == 'small_app_integration':
-            # # import ipdb; ipdb.set_trace()
-            # pass
-
-        # if job.name == 'main_full':
-            # import ipdb; ipdb.set_trace()
-
-
+        # for each job, successful_job_shas is a dictionary, mapping tuples
+        # of shas (ordered as per the job_sha_map) for successful builds to
+        # the build number in question
+        successful_job_shas[job] = {
+            result[1:]: result[0]
+            for result in query.values(*columns)
+        }
 
     pr_map = {}
-    for pr in open_prs:
+    for pr in open_pull_requests:
         project = pr.project
         pr_job_map = {}
 
         for job in project.jobs:
             sha_map = job_sha_map[job].copy()
             sha_map[project.name] = pr.head_commit
-            # pr_job_map[job] = sha_map
-            # interesting_shas.extend(sha_map.values())
-            count += 1
             shas = tuple(sha_map.values())
 
-            # if job.name == 'main_static_analysis' and pr.number == 1194:
-                # import ipdb; ipdb.set_trace()
-
-            # pr_job_map[job] = (shas in successful_job_shas[job])
-            successful_shas = successful_job_shas2[job]
+            successful_shas = successful_job_shas[job]
             try:
                 pr_job_map[job] = successful_shas[shas]
             except KeyError:
                 pr_job_map[job] = None
 
-            # if shas in successful_job_shas[job]:
-                # import ipdb; ipdb.set_trace()
-
         pr_map[pr] = pr_job_map
 
-    # import ipdb; ipdb.set_trace()
-    print count
-    print time.time() - start
     return pr_map
 
 
@@ -259,17 +188,9 @@ def _all_open_prs():
 def jenkins_check(pull_request):
     pr_map = all_open_prs()
 
-    # project = pull_request.project
-
-    # get all jobs relevant to this project and job type
-    # (i.e. figure out the dependencies/impact)
-    # jobs = _get_jobs(project.name)
     jobs = pr_map[pull_request].keys()
 
-    # shas = {
-        # project.name: pull_request.head_commit,
-    # }
-    # todo: one url per job
+    # TODO: one url per job
     url = url_for(
         'jenkins.pull_request_status',
         project_name=pull_request.project.name,
@@ -278,10 +199,7 @@ def jenkins_check(pull_request):
 
     check_statuses = []
 
-    # for each of the relevant jobs, find any build that matches the required
-    # set of SHAs and also passed
     for job in sorted(jobs, key=lambda j: j.name):
-        # status = False # has_successful_builds(job, shas)
         job_number = pr_map[pull_request][job]
         status = job_number is not None
         label = "Jenkins: {} [{}]".format(job.name, job_number)

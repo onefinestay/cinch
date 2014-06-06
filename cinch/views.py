6ad6b9b6c0d763af4658d522979913b6b41da78e
David Szotten <davidszotten@gmail.com>
2014-06-06 17:02:57 +0100

    dedicated pr view + show good to go list

--- cinch/views.py
+++ cinch/views.py
@@ -1,4 +1,4 @@
-from flask import g, render_template
+from flask import g, render_template, url_for
 import logging
 
 from cinch import app, db
@@ -40,12 +40,49 @@ def index():
     pulls = dbsession.query(PullRequest).filter(
         PullRequest.is_open == True).all()
     projects = dbsession.query(Project).all()
+    ready_pull_requests = []
     for pull in pulls:
         pull.checks = list(run_checks(pull))
         pull.sync_label = sync_label(pull.ahead_of_master, pull.behind_master)
+        pull.url = url_for(
+            'pull_request',
+            project_owner=pull.project.owner,
+            project_name=pull.project.name,
+            number=pull.number,
+        )
+        if (
+            all(check.status for check in pull.checks)
+            and pull.behind_master == 0
+        ):
+            ready_pull_requests.append(pull)
 
     return render_template(
-        'index.html', pull_requests=pulls, projects=projects)
+        'index.html',
+        pull_requests=pulls,
+        ready_pull_requests=ready_pull_requests,
+        projects=projects,
+    )
+
+
+@app.route('/pull_request/<project_owner>/<project_name>/<number>')
+@requires_auth
+def pull_request(project_owner, project_name, number):
+    session = db.session
+
+    pull_request = session.query(PullRequest).join(Project).filter(
+        PullRequest.number == number,
+        Project.owner == project_owner, Project.name == project_name
+    ).first()
+
+    if pull_request is None:
+        return "Unknown pull request", 404
+
+    pull_request.checks = list(run_checks(pull_request))
+    pull_request.sync_label = sync_label(
+        pull_request.ahead_of_master, pull_request.behind_master)
+
+    return render_template(
+        'pull_request.html', pull=pull_request)
 
 
 # test route

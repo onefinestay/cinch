61f6eb69303a277a64df4f721e0eb85d8000ef0c
David Szotten <davidszotten@gmail.com>
2014-06-09 18:41:12 +0100

    use public git url

--- tests/test_git.py
+++ tests/test_git.py
@@ -18,7 +18,9 @@ def tmp_base_dir(request):
 
 @pytest.fixture(scope='module')
 def repo(tmp_base_dir):
-    repo = Repo.setup_repo('onefinestay', 'cinch')
+    public_url_template = "https://github.com/{}/{}.git"
+    with patch('cinch.git', 'GITHUB_URL_TEMPLATE', public_url_template):
+        repo = Repo.setup_repo('onefinestay', 'cinch')
     return repo
 
 

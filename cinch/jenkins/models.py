72ff93a2585fc60bd95451b374dee36c2c139fbd
David Szotten <davidszotten@gmail.com>
2014-09-01 15:10:46 +0100

    model for job-projects to add parameter_name

--- cinch/jenkins/models.py
+++ cinch/jenkins/models.py
@@ -2,13 +2,12 @@ from cinch import db
 from cinch.models import STRING_LENGTH, Project
 
 
-job_projects = db.Table(
-    'job_projects',
-    db.Column('job_id', db.Integer, db.ForeignKey('jobs.id'),
-              primary_key=True),
-    db.Column('project_id', db.Integer, db.ForeignKey('projects.id'),
-              primary_key=True),
-)
+class JobProject(db.Model):
+    __tablename__ = 'job_projects'
+    job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'), primary_key=True)
+    project_id = db.Column(
+        db.Integer, db.ForeignKey('projects.id'), primary_key=True)
+    parameter_name = db.Column(db.String(STRING_LENGTH), nullable=True)
 
 
 class Job(db.Model):
@@ -17,7 +16,7 @@ class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(STRING_LENGTH), unique=True, nullable=False)
     projects = db.relationship(
-        'Project', secondary=job_projects, backref='jobs')
+        'Project', secondary=JobProject.__table__, backref='jobs')
 
     def ordered_projects(self):
         return sorted(list(self.projects), key=lambda p: p.name)
